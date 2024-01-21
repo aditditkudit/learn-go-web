e0b3d5fa388c51508663af9e95511bb39685e326
adit_ditkudit <adityaadit92@gmail.com>
2024-01-21 11:37:30 +0700

    learn http test

---
+++ http_test.go
@@ -0,0 +1,26 @@
+package learn_go_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "Hello World")
+}
+
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:9090/hehe", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
